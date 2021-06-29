9d624b54b7360220d0c6666f6549142ff54112f3
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-29 11:34:07 +0530

    Added get data

---
+++ src/elections_lk/tests/test_presidential.py
@@ -0,0 +1,25 @@
+"""Tests for elections_lk."""
+
+import unittest
+
+from elections_lk import presidential
+
+
+class TestCase(unittest.TestCase):
+    """Tests."""
+
+    def test_get_election_years(self):
+        """Test."""
+        election_years = presidential.get_election_years()
+        self.assertIn(1982, election_years)
+        self.assertIn(2019, election_years)
+
+    def test_get_election_data(self):
+        """Test."""
+        election_data = presidential.get_election_data(2019)
+        self.assertGreater(len(election_data), 160)
+        first_result = election_data[0]
+
+
+if __name__ == '__main__':
+    unittest.main()
