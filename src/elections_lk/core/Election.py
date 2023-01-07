61573556d4ee4bc7865a16a166c55c1952fb97c1
nuuuwan <nuuuwan@gmail.com>
2023-01-07 16:21:52 +0530

    refac: moved load

--- src/elections_lk/core/Election.py
+++ src/elections_lk/core/Election.py
@@ -1,6 +1,17 @@
 from dataclasses import dataclass
 
+from gig import ent_types
+
+from elections_lk.core import remote_data
+
 
 @dataclass
 class Election:
     year: int
+
+    @classmethod
+    def load(cls, year: int):
+        result_list = remote_data.get_result_list(
+            cls.election_type, year, ent_types.ENTITY_TYPE.PD
+        )
+        return cls(year, result_list)
