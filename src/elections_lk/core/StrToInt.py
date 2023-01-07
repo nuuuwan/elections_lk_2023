f5a350ee135e44d2b008fc0c1d3b121eb23f13b4
nuuuwan <nuuuwan@gmail.com>
2023-01-07 15:48:20 +0530

    feat: Added keys_sorted

--- src/elections_lk/core/StrToInt.py
+++ src/elections_lk/core/StrToInt.py
@@ -5,17 +5,43 @@ class StrToInt:
     def __getitem__(self, key: str) -> int:
         return self.d[key]
 
-    def __setitem__(self, key: str, value: int) -> None:
-        self.d[key] = value
-
-    def __delitem__(self, key: str) -> None:
-        del self.d[key]
-
     def __len__(self) -> int:
         return len(self.d)
 
     def __iter__(self):
         return iter(self.d)
 
+    def __contains__(self, key: str) -> bool:
+        return key in self.d
+
+    def keys(self):
+        return self.d.keys()
+
+    def values(self):
+        return self.d.values()
+
     def items(self):
-        return sorted(self.d.items(), key=lambda x: x[1], reverse=True)
+        return self.d.items()
+
+    def items_sorted(self):
+        return sorted(self.items(), key=lambda x: x[1], reverse=True)
+
+    def keys_sorted(self):
+        return [k for k, v in self.items_sorted()]
+
+    def __eq__(self, other):
+        if isinstance(other, StrToInt):
+            return self.d == other.d
+        if isinstance(other, dict):
+            return self.d == other
+        return False
+
+    @staticmethod
+    def concat(str_to_int_list):
+        d = {}
+        for str_to_int in str_to_int_list:
+            for k, v in str_to_int.items():
+                if k not in d:
+                    d[k] = 0
+                d[k] += v
+        return StrToInt(d)
--- tests/test_str_to_int.py
+++ tests/test_str_to_int.py
@@ -2,20 +2,27 @@ from unittest import TestCase
 
 from elections_lk.core.StrToInt import StrToInt
 
-TEST_STR_TO_INT = StrToInt({'a': 1, 'b': 2, 'c': 3})
-
 
 class TestStrToInt(TestCase):
     def test_init(self):
-        s = TEST_STR_TO_INT
-        s['d'] = 4
+        s = StrToInt({'a': 1, 'b': 2, 'c': 3})
         self.assertEqual(s['a'], 1)
         self.assertEqual(s['b'], 2)
         self.assertEqual(s['c'], 3)
-        self.assertEqual(s['d'], 4)
-        self.assertEqual(len(s), 4)
+        self.assertEqual(len(s), 3)
 
-    def test_sorted(self):
-        s = TEST_STR_TO_INT
-        s['d'] = 4
-        self.assertEqual(s.items(), [('d', 4), ('c', 3), ('b', 2), ('a', 1)])
+    def test_items_sorted(self):
+        s = StrToInt({'a': 1, 'b': 2, 'c': 3})
+        self.assertEqual(s.items_sorted(), [('c', 3), ('b', 2), ('a', 1)])
+
+    def test_keys_sorted(self):
+        s = StrToInt({'a': 1, 'b': 2, 'c': 3})
+        self.assertEqual(s.keys_sorted(), ['c', 'b', 'a'])
+
+    def test_concat(self):
+        s1 = StrToInt({'a': 1, 'b': 2, 'c': 3})
+        s2 = StrToInt({'a': 12, 'b': 23, 'c': 31})
+        self.assertEqual(
+            StrToInt.concat([s1, s2]),
+            StrToInt({'a': 13, 'b': 25, 'c': 34}),
+        )
