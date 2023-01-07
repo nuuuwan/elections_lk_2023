e1f966aca60c452ceacbb27a7ab36376233c5978
nuuuwan <nuuuwan@gmail.com>
2023-01-07 10:20:16 +0530

    feat: + filter_by_entity_type

--- src/elections_lk/core/remote_data.py
+++ src/elections_lk/core/remote_data.py
@@ -1,5 +1,6 @@
 import os
 
+from gig import ent_types
 from utils import timex, www
 from utils.cache import cache
 
@@ -15,5 +16,15 @@ def get_raw_result_list(election_type, year):
             GIG2_URL_ROOT,
             f'government-elections-{election_type}'
             + f'.regions-ec.{year}.tsv',
+        )
+    )
+
+
+def filter_by_entity_type(raw_result_list, entity_type):
+    return list(
+        filter(
+            lambda x: ent_types.get_entity_type(x['entity_id'])
+            == entity_type,
+            raw_result_list,
         )
     )
--- tests/test_remote_data.py
+++ tests/test_remote_data.py
@@ -1,11 +1,36 @@
 from unittest import TestCase
 
+from gig import ent_types
+
 from elections_lk.core import remote_data
 
 
 class TestRemoteData(TestCase):
-    def test_init(self):
+    def test_get_raw_result_list(self):
         raw_result_list = remote_data.get_raw_result_list(
             'presidential', 2019
         )
         self.assertEqual(len(raw_result_list), 14510)
+        keys = list(raw_result_list[0].keys())
+        for k in ['entity_id', 'valid', 'rejected', 'polled', 'electors']:
+            self.assertIn(k, keys)
+
+    def test_filter_by_entity_type(self):
+        raw_result_list = remote_data.get_raw_result_list(
+            'presidential', 2019
+        )
+        for entity_type, n_results in [
+            [ent_types.ENTITY_TYPE.PD, 160],
+            [ent_types.ENTITY_TYPE.ED, 22],
+            [ent_types.ENTITY_TYPE.COUNTRY, 1],
+        ]:
+            filtered_raw_result_list = remote_data.filter_by_entity_type(
+                raw_result_list,
+                entity_type,
+            )
+            self.assertEqual(len(filtered_raw_result_list), n_results)
+            first_result = filtered_raw_result_list[0]
+            self.assertEqual(
+                ent_types.get_entity_type(first_result['entity_id']),
+                entity_type,
+            )
