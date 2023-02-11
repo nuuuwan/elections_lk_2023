263fa13cf3f06aac4278af73bf3fce90f93af877
nuuuwan <nuuuwan@gmail.com>
2023-02-11 14:33:41 +0530

    feat: MultiStepSankey #STUB

---
+++ src/elections_lk/sankey/MultiStepSankey.py
@@ -0,0 +1,50 @@
+from functools import cached_property
+
+from utils import Log
+
+# ElectionParliamentary, ElectionPresidential
+from elections_lk import ElectionParliamentary, ElectionPresidential
+from elections_lk.sankey.SankeyDraw import SankeyDraw
+from elections_lk.sankey.SankeyModel import SankeyModel
+from elections_lk.sankey.SankeyModelNormalize import SankeyModelNormalize
+from elections_lk.sankey.SankeyReport import SankeyReport
+
+log = Log('PartyContinuity')
+
+
+class MultiStepSankey:
+    def __init__(self, election_list: List[Electoin]):
+        self.election_list = election_list
+
+    def draw(self):
+        pass
+
+
+if __name__ == '__main__':
+    election_list = [
+        # 1980s
+        # ElectionPresidential.from_year(1982),
+        # ElectionPresidential.from_year(1988),
+        # ElectionParliamentary.from_year(1989),
+        # 1990s
+        # ElectionParliamentary.from_year(1994),
+        # ElectionPresidential.from_year(1994),
+        # ElectionPresidential.from_year(1999),
+        # 2000s
+        # ElectionParliamentary.from_year(2000),
+        # ElectionParliamentary.from_year(2001),
+        # ElectionParliamentary.from_year(2004),
+        # ElectionPresidential.from_year(2005),
+        # 2010s
+        # ElectionPresidential.from_year(2010),
+        # ElectionParliamentary.from_year(2010),
+        # ElectionPresidential.from_year(2015),
+        # ElectionParliamentary.from_year(2015),
+        # ElectionLocalAuthority.from_year(2018),
+        ElectionPresidential.from_year(2019),
+        # 2020s
+        ElectionParliamentary.from_year(2020),
+    ]
+
+    s = MultiStepSankey(election_list)
+    s.draw()
