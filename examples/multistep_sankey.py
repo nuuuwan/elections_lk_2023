11c1c5e392edbcc3bacbfd4401f1c4067c6906f7
nuuuwan <nuuuwan@gmail.com>
2025-11-03 08:50:36 +0530

    Moved multistep_sankey to examples

---
+++ examples/multistep_sankey.py
@@ -0,0 +1,42 @@
+from elections_lk import (
+    ElectionParliamentary,
+    ElectionPresidential,
+    MultiStepSankey,
+)
+
+if __name__ == "__main__":
+    for election_list, title in [
+        [
+            [
+                # 1980s
+                ElectionPresidential.from_year(1982),
+                ElectionPresidential.from_year(1988),
+                ElectionParliamentary.from_year(1989),
+                # 1990s
+                ElectionParliamentary.from_year(1994),
+                ElectionPresidential.from_year(1994),
+                ElectionPresidential.from_year(1999),
+                # 2000s
+                ElectionParliamentary.from_year(2000),
+                ElectionParliamentary.from_year(2001),
+                ElectionParliamentary.from_year(2004),
+                ElectionPresidential.from_year(2005),
+                # 2010s
+                ElectionPresidential.from_year(2010),
+                ElectionParliamentary.from_year(2010),
+                ElectionPresidential.from_year(2015),
+                ElectionParliamentary.from_year(2015),
+                ElectionPresidential.from_year(2019),
+                # 2020s
+                ElectionParliamentary.from_year(2020),
+                ElectionPresidential.from_year(2024),
+                ElectionParliamentary.from_year(2024),
+            ],
+            "Presidential and Parliamentary Elections (1982-)",
+        ],
+    ]:
+        mss = MultiStepSankey(
+            election_list,
+            title,
+        )
+        mss.draw()
--- src/elections_lk/sankey/MultiStepSankey.py
+++ src/elections_lk/sankey/MultiStepSankey.py
@@ -2,11 +2,12 @@ from functools import cached_property
 
 from utils import Log
 
-# ElectionParliamentary, ElectionPresidential, ElectionLocalAuthority
-from elections_lk import ElectionParliamentary, ElectionPresidential
 from elections_lk.sankey.Sankey import Sankey
 from elections_lk.sankey.SankeyBase import SankeyBase
 from elections_lk.sankey.SankeyDrawMixin import SankeyDraw
+
+# ElectionParliamentary, ElectionPresidential, ElectionLocalAuthority
+
 
 log = Log("MultiStepSankey")
 
@@ -21,41 +22,3 @@ class MultiStepSankey(SankeyDraw, SankeyBase):
             sankey = Sankey([election_x, election_y])
             matrices.append(sankey.matrix)
         return matrices
-
-
-if __name__ == "__main__":
-    for election_list, title in [
-        [
-            [
-                # 1980s
-                ElectionPresidential.from_year(1982),
-                ElectionPresidential.from_year(1988),
-                ElectionParliamentary.from_year(1989),
-                # 1990s
-                ElectionParliamentary.from_year(1994),
-                ElectionPresidential.from_year(1994),
-                ElectionPresidential.from_year(1999),
-                # 2000s
-                ElectionParliamentary.from_year(2000),
-                ElectionParliamentary.from_year(2001),
-                ElectionParliamentary.from_year(2004),
-                ElectionPresidential.from_year(2005),
-                # 2010s
-                ElectionPresidential.from_year(2010),
-                ElectionParliamentary.from_year(2010),
-                ElectionPresidential.from_year(2015),
-                ElectionParliamentary.from_year(2015),
-                ElectionPresidential.from_year(2019),
-                # 2020s
-                ElectionParliamentary.from_year(2020),
-                ElectionPresidential.from_year(2024),
-                ElectionParliamentary.from_year(2024),
-            ],
-            "Presidential and Parliamentary Elections (1982-)",
-        ],
-    ]:
-        mss = MultiStepSankey(
-            election_list,
-            title,
-        )
-        mss.draw()
