8ca18fd1f96ce18eba5ba5599d9443e9aa1dcbba
nuuuwan <nuuuwan@gmail.com>
2025-11-03 14:11:12 +0530

    Added one more election

--- examples/sankey.py
+++ examples/sankey.py
@@ -7,6 +7,11 @@ if __name__ == "__main__":
             ElectionPresidential.from_year(2015),
             "2005 and 2015 Sri Lankan Presidential Elections",
         ],
+        [
+            ElectionPresidential.from_year(2019),
+            ElectionPresidential.from_year(2024),
+            "2019 and 2024 Sri Lankan Presidential Elections",
+        ],
     ]:
 
         s = Sankey(election_x, election_y, title)
